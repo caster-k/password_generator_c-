9df15d651a24c9234ccccdf2ce430b4bcf85f6be
caster-k <castergastino@gmail.com>
2025-02-04 21:22:48 +0545

    password generator without saving the password in any places

--- password_generator_c++.cpp
+++ password_generator_c++.cpp
@@ -1,35 +1,14 @@
 #include<iostream>
 #include<ctime>
 #include<string>
-using namespace std;
-// string password_generator_using_find();
-string passwordGenerator();
 
+using namespace std;
+string passwordGenerator();
 string allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW1234567890@#$%^&(),;<=>?";
 string unique_characters = "";
 
-
 int main(){
-    char ch = '1';
-    // cout<<unique_characters.find(ch)<<endl;
-    // if(unique_characters.find(ch)) {
-    //     cout<<"Character found in allCharacters!"<<endl;
-    // }
-    // else {
-    //     cout<<"Character not found in allCharacters!"<<endl;
-    
-    // }
-    cout<<"\n\n The password is: "<<passwordGenerator()<<endl;
-    // string password;
-    // srand(time(0));
-    // // cout<<allCharacters[rand()%allCharacters.size()
-    // cout<<allCharacters.length()<<endl;
-
-    // for(int i = 0; i<20 ; i++){
-
-    //     password = password + allCharacters[rand()%(allCharacters.length()-1)];
-    // }
-    // cout<<password<<endl;
+    cout<<"\nThe password is: "<<passwordGenerator()<<endl;
 }
 
 string passwordGenerator(){
@@ -44,44 +23,14 @@ string passwordGenerator(){
                 break;
             }
         }
-        // for(int j = 0; j<=unique_characters.length(); j++){
-            // cout<<generatedCharacter<<endl;
-            if(uniqueCharacterFlag){
-                password = password + generatedCharacter;
-                unique_characters += generatedCharacter;
-                // break;
-                // cout<<generatedCharacter<<endl;
-            }
-            else{
-                // cout<<"khai k pa hudai xa"<<endl;
-                i--;
-                // break;
-            }
-    // }
+        
+        if(uniqueCharacterFlag){
+            password = password + generatedCharacter;
+            unique_characters += generatedCharacter;
+        }
+        else{
+            i--;
+        }
     }
-    // cout<<password<<endl;
     return password;
 }
-
-// string password_generator_using_find(){
-//     string password = "";
-//     srand(time(0));
-//     for(int i = 0; i<20 ; i++){
-//         char ch = allCharacters[rand()%(allCharacters.length()-1)];
-        
-//         if (!(unique_characters.find(ch))){
-//             cout<<ch<<endl;
-//             password = password + ch;
-//             unique_characters += ch;
-//             continue;
-//         }
-//         else{
-//             i--;
-//             continue;
-//         }
-//     }
-//     return password;
-// }
-// string remove_duplicates(string password){
-
-// }
